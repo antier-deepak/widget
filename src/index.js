cca563eae836541aef5a249751eafcecfcd5f38a
deepak <deepak2@antiersolutions.com>
2023-05-04 10:36:42 +0530

    widget v1

--- src/index.js
+++ src/index.js
@@ -1,15 +1,31 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// const root = ReactDOM.createRoot(document.getElementById("widget"));
+
+window.ReactCounter = {
+  mount: (props) => {
+    const el = document.getElementById("widget");
+    const root = ReactDOM.createRoot(el);
+    root.render(<App {...props} />, el);
+  },
+  unmount: () => {
+    const el = document.getElementById("widget");
+    ReactDOM.unmountComponentAtNode(el);
+  },
+};
+
+// Find all widget divs
+// const widgetDiv = ReactDOM.createRoot(document.getElementById(".dummy-widget"));
+// console.log("widget running!");
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
