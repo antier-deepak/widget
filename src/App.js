018af4a424c9f61cb571c4980d5593905d2f4ede
deepak <deepak2@antiersolutions.com>
2023-05-10 12:42:38 +0530

    api added

--- src/App.js
+++ src/App.js
@@ -1,19 +1,40 @@
 import "./App.css";
-import { Box, Modal, Typography } from "@mui/material";
+import { Box, Container, Grid, Modal, Typography } from "@mui/material";
 import { useEffect, useState } from "react";
 import { Button } from "@mui/base";
+import CampaignCard from "./components/CampaignCard/CampaignCard";
 
 function App(props) {
-  const { title, counterData } = props;
+  // const { title, counterData } = props;
   const [show, setShow] = useState(true);
-  const [count, setCount] = useState(0);
+  const [campaigns, setCampaigns] = useState("");
+  const [donationSuccess, setDonationSuccess] = useState(false);
+
   const handleClose = () => {
     setShow(false);
   };
 
+  const getCollections = () => {
+    fetch("https://qa-api.stumpup.org/user/api/v1/users/campaign/list/2/1", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify({}), // body data type must match "Content-Type" header
+    })
+      .then((res) => {
+        return res?.json();
+      })
+      .then((data) => {
+        console.log("data", data);
+        setCampaigns(data?.data?.rows);
+      });
+  };
+
   useEffect(() => {
-    counterData(count);
-  }, [count, counterData]);
+    getCollections();
+  }, []);
 
   const style = {
     position: "absolute",
@@ -26,6 +47,54 @@ function App(props) {
     boxShadow: 24,
     p: 4,
   };
+
+  const buttonClickHandler = (campaign) => {
+    let data = {
+      campaign_id: campaign?.id,
+      amount: 1,
+      coin: "matic",
+    };
+
+    fetch("https://qa-api.stumpup.org/user/api/v1/users/donation/donate", {
+      method: "POST", // *GET, POST, PUT, DELETE, etc.
+      headers: {
+        "Content-Type": "application/json",
+        authorization:
+          "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjgwYmU4MDQ3LWY0M2UtNDM5My04YTI0LTViZWFmODY5MWY5OCIsInJvbGUiOiJmdW5kcmFpc2VyIiwiaWF0IjoxNjgzNjI5OTk5LCJleHAiOjE2ODM3MTYzOTl9.eUMEZXwQ5cdIKKNcKpwiJtZgVSk9DtXb7VpinIYJM4M",
+
+        // 'Content-Type': 'application/x-www-form-urlencoded',
+      },
+      body: JSON.stringify(data), // body data type must match "Content-Type" header
+    })
+      .then((res) => {
+        return res?.json();
+      })
+      .then((data) => {
+        console.log("data", data);
+        if (data?.error) {
+          setDonationSuccess(true);
+        }
+      });
+
+    // await apiCallPost(
+    //   "",
+    //   {
+    //     campaign_id: campData?.id,
+    //     amount: amount,
+    //     coin: "matic",
+    //   },
+    //   {},
+    //   true
+    // )
+    //   .then((results) => {
+    //     if (!results.error) {
+    //       handleDonationSuccessModal();
+    //       // handleClose();
+    //       CrowdfundingData();
+    //     }
+    //   })
+    //   .catch((error) => {});
+  };
   return (
     <Modal
       open={show}
@@ -34,33 +103,36 @@ function App(props) {
       aria-describedby="modal-modal-description"
     >
       <Box sx={style}>
-        <Typography id="modal-modal-title" variant="h6" component="h2">
-          {title}
-        </Typography>
-        <Typography id="modal-modal-description" sx={{ mt: 2 }}>
-          {count}
-        </Typography>
-        <Button onClick={() => setCount(count + 1)}>Increment</Button>
-        <Button onClick={() => setCount(count - 1)}>Decrement</Button>
+        {donationSuccess ? (
+          <div style={{ textAlign: "center" }}>
+            <Typography variant="h6" component="h2">
+              Donation Successfull!
+            </Typography>
+            <Button onClick={() => setDonationSuccess(false)}>Back</Button>
+          </div>
+        ) : (
+          <>
+            <Typography id="modal-modal-title" variant="h6" component="h2">
+              Campaigns
+            </Typography>
+            {campaigns?.length > 0 && (
+              <Grid container spacing={2}>
+                {campaigns?.map((item) => {
+                  return (
+                    <Grid item key={item?.id}>
+                      <CampaignCard
+                        data={item}
+                        buttonClickHandler={() => buttonClickHandler(item)}
+                      />
+                    </Grid>
+                  );
+                })}
+              </Grid>
+            )}
+          </>
+        )}
       </Box>
     </Modal>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
---
+++ src/components/CampaignCard/CampaignCard.js
@@ -0,0 +1,31 @@
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  Typography,
+} from "@mui/material";
+import React from "react";
+import "./CampaignCard.css";
+
+const CampaignCard = ({ data, buttonClickHandler }) => {
+  return (
+    <Card sx={{ minWidth: 275 }} className="campaignCard">
+      <CardContent>
+        <Typography sx={{ fontSize: 14 }} color="text.secondary" gutterBottom>
+          {data?.title}
+        </Typography>
+        <Typography sx={{ mb: 1.5 }} color="text.secondary">
+          {data?.collectionName}
+        </Typography>
+        <Typography variant="body2">{data?.description}</Typography>
+      </CardContent>
+
+      <Button size="small" onClick={buttonClickHandler}>
+        Donate
+      </Button>
+    </Card>
+  );
+};
+
+export default CampaignCard;
